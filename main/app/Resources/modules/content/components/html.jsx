27f14ba8e99b422b3734ebfc600061175d8b8de5
Nicolas Pavie <nicolas.pavie@braillenet.org>
2020-11-24 17:14:01 +0100

    Anchor links support in html content
    Replacing anchor links by HashLink components before rendering html
    content.
    
    HashLink elements are then converted by the JsxParser component to
    correct anchor links that can be used by the application HashRouter
    component.

--- main/app/Resources/modules/content/components/html.jsx
+++ main/app/Resources/modules/content/components/html.jsx
@@ -3,18 +3,34 @@ import {PropTypes as T} from 'prop-types'
 import classes from 'classnames'
 import omit from 'lodash/omit'
 
+
+import JsxParser from 'react-jsx-parser';
+import { HashLink } from 'react-router-hash-link';
+
+
+
 /**
  * Interprets and displays HTML content.
  *
  * @param props
  * @constructor
  */
-const ContentHtml = props =>
-  <div
-    {...omit(props, 'children')}
-    className={classes('text-html-content text-justify', props.className)}
-    dangerouslySetInnerHTML={{ __html: props.children }}
-  />
+const ContentHtml = props => {
+    const anchorsMatcher = RegExp(/<a[^>]*href="#[^\/"]*"[^>]*>.*<\/a>/gi);
+    var contentWithHashLinks = props.children.replace(anchorsMatcher, (match) => {
+      return match.replace('<a','<HashLink')
+        .replace('/a>','/HashLink>')
+        .replace('href=','to=');
+    });
+
+    return <div
+        {...omit(props, 'children')}
+        className={classes('text-html-content text-justify', props.className)}>
+      <JsxParser 
+        components={{ HashLink }}
+        jsx={contentWithHashLinks}/>
+    </div>;
+  }
 
 ContentHtml.propTypes = {
   /**
